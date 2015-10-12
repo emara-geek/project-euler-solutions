1fa2a0485b89d1ea3e0e6567284bf03d7cd81fae
Taha Emara <tahaemara.eng@gmail.com>
2015-10-12 23:37:16 +0300

    Update EulerProject4.java

--- EulerProject4.java
+++ EulerProject4.java
@@ -7,8 +7,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- *
- * @author Taha Emara
+ * @author Taha Emara 
+ * Youtube Cahnnel  : http://www.youtube.com/user/omar0103637
+ * Facebook Page    : https://www.facebook.com/IcPublishes
+ * Linkedin Profile : http://eg.linkedin.com/pub/taha-emara/a4/1ab/524/
  */
 public class EulerProject4 {
 
