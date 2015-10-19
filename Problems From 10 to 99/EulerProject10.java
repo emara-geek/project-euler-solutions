f4242b3afbfe2babf0b0ff8e49cb223656cc1d79
Taha Emara <tahaemara.eng@gmail.com>
2015-10-19 12:37:25 +0300

    Update EulerProject10.java

--- Problems From 10 to 99/EulerProject10.java
+++ Problems From 10 to 99/EulerProject10.java
@@ -1,6 +1,5 @@
 /*
  Problem 10
- Published on Friday, 8th February 2002, 08:00 pm; Solved by 192064; Difficulty rating: 5%
  The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
  Find the sum of all the primes below two million.
  link to the problem https://projecteuler.net/problem=10
